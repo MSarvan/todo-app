e8534ed0ed4186c465bf8dd6e51a014c561f3db5
Saravanan M <sarvan.m20@gmail.com>
2024-05-08 17:19:25 +0530

    updated the app

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import EditTodoForm from "./EditTodoForm";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoWrapper = () => {
-  return (
-    <div>TodoWrapper</div>
-  )
-}
+  const [todos, setTodos] = useState([]);
 
-export default TodoWrapper
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <div className="heading">
+        <img
+          src="https://cdn-icons-png.freepik.com/256/11382/11382348.png?semt=ais_hybrid"
+          alt=""
+          className="heroImg"
+        />
+        <h1>Get Things Done !</h1>
+      </div>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default TodoWrapper;
